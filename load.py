cb6f6c3dec14499f7ae0c92bc8012eb2de01fe7f
Ivan Baksheev <zpix-dev@list.ru>
2021-05-23 15:03:46 +0700

    test

--- load.py
+++ load.py
@@ -6,6 +6,7 @@ client = NotionClientWhoCanDownload(token_v2=os.environ.get('NOTION_V2'))
 page = client.get_block("https://www.notion.so/zpix/NSU-DOCS-761a53f14fb048d4af34f68ad49eaaf9")
 
 for child in page.children:
+    print(f'Loading {child.title}')
     pathlib.Path('pdf').mkdir(parents=True, exist_ok=True)
     path = f'pdf/{child.title}-{child.id}.pdf'
     client.download_block(child.id, path, export_type='pdf')
